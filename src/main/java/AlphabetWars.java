859c996e223e59e31cb030d09b86af55c2f0c3bd
cduggin <cduggin@parchment.com>
2020-06-11 09:54:39 -0700

    initial checkin

---
+++ src/main/java/AlphabetWars.java
@@ -0,0 +1,8 @@
+public class AlphabetWars {
+
+    public static String woLoLoooooo(String battlefield) {
+        // Your code here!
+        return null;
+    }
+
+}
---
+++ src/test/java/AlphabetWarsTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AlphabetWarsTest {
+
+    @Test
+    public void woLoLoooooo_WRITE_TEST_NAME_HERE() {
+        assertEquals("",   AlphabetWars.woLoLoooooo(""));
+    }
+
+}
