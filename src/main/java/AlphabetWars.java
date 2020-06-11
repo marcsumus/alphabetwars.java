f81f8eba2d502e493085c377cc1c2f516ad9c615
cduggin <cduggin@parchment.com>
2020-06-11 12:05:39 -0700

    Round 1

--- src/main/java/AlphabetWars.java
+++ src/main/java/AlphabetWars.java
@@ -1,8 +1,26 @@
 public class AlphabetWars {
 
     public static String woLoLoooooo(String battlefield) {
-        // Your code here!
-        return null;
+
+        //Single Letter Battles
+        if (battlefield.equals("z") || battlefield.equals("d") || battlefield.equals("q") || battlefield.equals("m")) {
+            return "Right side wins!";
+        }
+
+        if (battlefield.equals("s") || battlefield.equals("b") || battlefield.equals("p") || battlefield.equals("w")) {
+            return "Left side wins!";
+        }
+
+        //Multi Letter Battles
+        if (battlefield.equals("ds")) {
+            return "Right side wins!";
+        }
+
+        if(battlefield.equals("wm")) {
+            return "Let's fight again!";
+        }
+
+        return "Left side wins!";
     }
 
 }
--- src/test/java/AlphabetWarsTest.java
+++ src/test/java/AlphabetWarsTest.java
@@ -3,10 +3,64 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class AlphabetWarsTest {
+    //Right
+    @Test
+    public void woLoLoooooo_When_z_Then_RightSideWins() {
+        assertEquals("Right side wins!",   AlphabetWars.woLoLoooooo("z"));
+    }
 
     @Test
-    public void woLoLoooooo_WRITE_TEST_NAME_HERE() {
-        assertEquals("",   AlphabetWars.woLoLoooooo(""));
+    public void woLoLoooooo_When_d_Then_RightSideWins() {
+        assertEquals("Right side wins!",   AlphabetWars.woLoLoooooo("d"));
     }
+
+    @Test
+    public void woLoLoooooo_When_q_Then_RightSideWins() {
+        assertEquals("Right side wins!",   AlphabetWars.woLoLoooooo("q"));
+    }
+
+    @Test
+    public void woLoLoooooo_When_m_Then_RightSideWins() {
+        assertEquals("Right side wins!",   AlphabetWars.woLoLoooooo("m"));
+    }
+
+    //Left
+    @Test
+    public void woLoLoooooo_When_s_Then_LeftSideWins() {
+        assertEquals("Left side wins!",   AlphabetWars.woLoLoooooo("s"));
+    }
+
+    @Test
+    public void woLoLoooooo_When_b_Then_LeftSideWins() {
+        assertEquals("Left side wins!",   AlphabetWars.woLoLoooooo("b"));
+    }
+
+    @Test
+    public void woLoLoooooo_When_p_Then_LeftSideWins() {
+        assertEquals("Left side wins!",   AlphabetWars.woLoLoooooo("p"));
+    }
+
+    @Test
+    public void woLoLoooooo_When_w_Then_LeftSideWins() {
+        assertEquals("Left side wins!",   AlphabetWars.woLoLoooooo("w"));
+    }
+
+    //Battles
+    @Test
+    public void woLoLoooooo_When_bz_Then_LeftSideWins() {
+        assertEquals("Left side wins!",   AlphabetWars.woLoLoooooo("bz"));
+    }
+
+    @Test
+    public void woLoLoooooo_When_ds_Then_RightSideWins() {
+        assertEquals("Right side wins!",   AlphabetWars.woLoLoooooo("ds"));
+    }
+
+    @Test
+    public void woLoLoooooo_When_wm_Then_LetsFightAgain() {
+        assertEquals("Let's fight again!",   AlphabetWars.woLoLoooooo("wm"));
+    }
+
+
 
 }
